d87786867486dcfd39c20748ebdfc5a1d0d184de
Chelsea Spangler <chelseanspangler@gmail.com>
2020-02-26 17:12:37 -0800

    set up user authorization

--- server.js
+++ server.js
@@ -9,6 +9,31 @@ const client = require('./lib/client');
 // Initiate database connection
 client.connect();
 
+// Auth
+const ensureAuth = require('./lib/auth/ensure-auth');
+const createAuthRoutes = require('./lib/auth/create-auth-routes');
+const authRoutes = createAuthRoutes({
+    selectUser(email) {
+        return client.query(`
+            SELECT id, email, hash 
+            FROM users
+            WHERE email = $1;
+        `,
+        [email]
+        ).then(result => result.rows[0]);
+    },
+    insertUser(user, hash) {
+        console.log(user);
+        return client.query(`
+            INSERT into users (email, hash)
+            VALUES ($1, $2, $3)
+            RETURNING id, email;
+        `,
+        [user.email, hash]
+        ).then(result => result.rows[0]);
+    }
+});
+
 // Application Setup
 const app = express();
 const PORT = process.env.PORT;
@@ -17,7 +42,17 @@ app.use(cors()); // enable CORS request
 app.use(express.static('public')); // server files from /public folder
 app.use(express.json()); // enable reading incoming json data
 app.use(express.urlencoded({ extended: true }));
+
+// Set up authentication routes
+app.use('/api/auth', authRoutes);
+app.use('/api', ensureAuth);
+
 // API Routes
+app.get('/api/test', (req, res) => {
+    res.json({
+        message: `the user's id is ${req.userId}`
+    });
+});
 
 // *** TODOS ***
 app.get('/api/todos', async(req, res) => {
